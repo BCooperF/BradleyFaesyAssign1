524ecb9c9fbaaa29e8f830c2e8ed9142543d0cf5
BCooperF <bradley_faesy@mymail.eku.edu>
2026-02-03 15:42:51 -0500

    Initial Commit

---
+++ BradleyFaesyAssign1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BradleyFaesyAssign1
+//
+//  Created by Faesy, Bradley C. on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
